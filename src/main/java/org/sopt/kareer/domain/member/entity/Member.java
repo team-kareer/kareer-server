c1aa82c662e2873dd15b4d4d0278f889fe0d11f7
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-04 20:55:11 +0900

    [Feat] #3 Member 엔티티 구현

---
+++ src/main/java/org/sopt/kareer/domain/member/entity/Member.java
@@ -0,0 +1,63 @@
+package org.sopt.kareer.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.kareer.global.entity.BaseEntity;
+
+import java.time.LocalDate;
+
+@Table(name = "members")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String birthDate;
+
+    // 추후에 enum 사용 가능
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String primaryMajor;
+
+    private String secondaryMajor;
+
+    @Column(nullable = false)
+    private String targetJob;
+
+    @Column(nullable = false)
+    private LocalDate graduationDate;
+
+    private LocalDate expectedGraduationDate;
+
+    private String personalBackground;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VisaType visaType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VisaStatus visaStatus;
+
+    private LocalDate visaExpiredAt;
+
+    private String university;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LanguageLevel languageLevel;
+
+}
