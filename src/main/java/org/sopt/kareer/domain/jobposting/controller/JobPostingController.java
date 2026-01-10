db77e263015cd4decc144d37b8d0a6ae33e13bc4
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-10 19:24:26 +0900

    [Chore] #11 JobPostingController 엔드포인트 변경

--- src/main/java/org/sopt/kareer/domain/jobposting/controller/JobPostingController.java
+++ src/main/java/org/sopt/kareer/domain/jobposting/controller/JobPostingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("jobpostings")
+@RequestMapping("job-postings")
 public class JobPostingController {
 
     private final JobPostingCrawler jobPostingCrawler;
