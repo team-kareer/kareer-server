684c2a826d31459df8eb4315fb7a9dfa3ff0054b
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-12 23:59:05 +0900

    [Feat] #10 Document 에러코드, 커스텀 예외 클래스 구현

---
+++ src/main/java/org/sopt/kareer/domain/exception/DocumentException.java
@@ -0,0 +1,14 @@
+package org.sopt.kareer.domain.exception;
+
+import org.sopt.kareer.global.exception.customexception.CustomException;
+import org.sopt.kareer.global.exception.errorcode.ErrorCode;
+
+public class DocumentException extends CustomException {
+    public DocumentException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+
+    public DocumentException(ErrorCode errorCode){
+      super(errorCode);
+    }
+}
