9ffa16bc6deb347e6f0f1ecedfbbae97529f0179
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-12 23:59:56 +0900

    [Feat] #10 PDF 문서 업로드 및 임베딩 컨트롤러 로직 구현

--- src/main/java/org/sopt/kareer/global/config/swagger/SwaggerResponseDescription.java
+++ src/main/java/org/sopt/kareer/global/config/swagger/SwaggerResponseDescription.java
@@ -1,15 +1,22 @@
 package org.sopt.kareer.global.config.swagger;
 
 import lombok.Getter;
+import org.sopt.kareer.domain.exception.DocumentErrorCode;
 import org.sopt.kareer.global.exception.errorcode.ErrorCode;
 import org.sopt.kareer.global.exception.errorcode.GlobalErrorCode;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static org.sopt.kareer.domain.exception.DocumentErrorCode.*;
+
 @Getter
 public enum SwaggerResponseDescription {
     DEFAULT(new LinkedHashSet<>(Set.of(
+    ))),
+    UPLOAD_PDF(new LinkedHashSet<>(Set.of(
+            DOCUMENT_PROCESSING_FAILED,
+            EXTRACT_TEXT_FAILED
     )))
 
     ;
