71d8d568ea47f9ef45a63b4156eed506361e911d
Dohun Kim <dohun8758@naver.com>
2026-01-12 22:23:20 +0900

    [Feat] #40 회원 정보 응답 DTO 추가

---
+++ src/main/java/org/sopt/kareer/domain/member/dto/response/MemberInfoResponse.java
@@ -0,0 +1,41 @@
+package org.sopt.kareer.domain.member.dto.response;
+
+import java.time.LocalDate;
+import org.sopt.kareer.domain.member.entity.Member;
+import org.sopt.kareer.domain.member.entity.enums.Country;
+import org.sopt.kareer.domain.member.entity.enums.Degree;
+import org.sopt.kareer.domain.member.entity.enums.LanguageLevel;
+
+public record MemberInfoResponse(
+        Long memberId,
+        String name,
+        String profileImageUrl,
+        LocalDate birthDate,
+        Country country,
+        String primaryMajor,
+        String secondaryMajor,
+        String targetJob,
+        LocalDate graduationDate,
+        LocalDate expectedGraduationDate,
+        LanguageLevel languageLevel,
+        Degree degree,
+        String targetJobSkill
+) {
+    public static MemberInfoResponse fromEntity(Member member) {
+        return new MemberInfoResponse(
+                member.getId(),
+                member.getName(),
+                member.getProfileImageUrl(),
+                member.getBirthDate(),
+                member.getCountry(),
+                member.getPrimaryMajor(),
+                member.getSecondaryMajor(),
+                member.getTargetJob(),
+                member.getGraduationDate(),
+                member.getExpectedGraduationDate(),
+                member.getLanguageLevel(),
+                member.getDegree(),
+                member.getTargetJobSkill()
+        );
+    }
+}
