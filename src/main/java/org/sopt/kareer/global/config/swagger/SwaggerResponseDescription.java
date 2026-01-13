1d7de5368b6228d18ac8223ccc0442296ff27bc0
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-13 22:29:49 +0900

    [Chore] #45 스웨거 명세 Enum 변경

--- src/main/java/org/sopt/kareer/global/config/swagger/SwaggerResponseDescription.java
+++ src/main/java/org/sopt/kareer/global/config/swagger/SwaggerResponseDescription.java
@@ -1,22 +1,22 @@
 package org.sopt.kareer.global.config.swagger;
 
 import lombok.Getter;
-import org.sopt.kareer.domain.exception.DocumentErrorCode;
 import org.sopt.kareer.global.exception.errorcode.ErrorCode;
 import org.sopt.kareer.global.exception.errorcode.GlobalErrorCode;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import static org.sopt.kareer.domain.exception.DocumentErrorCode.*;
+import static org.sopt.kareer.domain.document.exception.DocumentErrorCode.EMBEDDING_FAILED;
+import static org.sopt.kareer.domain.document.exception.DocumentErrorCode.EXTRACT_TEXT_FAILED;
 
 @Getter
 public enum SwaggerResponseDescription {
     DEFAULT(new LinkedHashSet<>(Set.of(
     ))),
     UPLOAD_PDF(new LinkedHashSet<>(Set.of(
-            DOCUMENT_PROCESSING_FAILED,
-            EXTRACT_TEXT_FAILED
+            EXTRACT_TEXT_FAILED,
+            EMBEDDING_FAILED
     )))
 
     ;
