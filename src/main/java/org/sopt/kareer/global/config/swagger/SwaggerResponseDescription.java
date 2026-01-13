25a7ac9ac4f19aa32f579b3abe7772d87bf701ad
Dohun Kim <dohun8758@naver.com>
2026-01-13 22:34:04 +0900

    [Feat] #40 Swagger 응답 설명에 회원 관련 에러 코드 추가

--- src/main/java/org/sopt/kareer/global/config/swagger/SwaggerResponseDescription.java
+++ src/main/java/org/sopt/kareer/global/config/swagger/SwaggerResponseDescription.java
@@ -1,5 +1,9 @@
 package org.sopt.kareer.global.config.swagger;
 
+import static org.sopt.kareer.global.exception.errorcode.MemberErrorCode.MEMBER_NOT_FOUND;
+import static org.sopt.kareer.global.exception.errorcode.MemberErrorCode.ONBOARDING_ALREADY_COMPLETED;
+import static org.sopt.kareer.global.exception.errorcode.MemberErrorCode.ONBOARDING_REQUIRED;
+
 import lombok.Getter;
 import org.sopt.kareer.global.exception.errorcode.ErrorCode;
 import org.sopt.kareer.global.exception.errorcode.GlobalErrorCode;
@@ -10,8 +14,15 @@ import java.util.Set;
 @Getter
 public enum SwaggerResponseDescription {
     DEFAULT(new LinkedHashSet<>(Set.of(
-    )))
-
+    ))),
+    MEMBER_INFO(new LinkedHashSet<>(Set.of(
+            MEMBER_NOT_FOUND,
+            ONBOARDING_REQUIRED
+    ))),
+    MEMBER_ONBOARD(new LinkedHashSet<>(Set.of(
+            MEMBER_NOT_FOUND,
+            ONBOARDING_ALREADY_COMPLETED
+    ))),
     ;
     private final Set<ErrorCode> errorCodeList;
     SwaggerResponseDescription(Set<ErrorCode> specificErrorCodes) {
