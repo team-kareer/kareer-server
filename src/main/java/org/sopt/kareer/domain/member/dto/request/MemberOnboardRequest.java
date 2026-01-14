5a3f674fd81bc024f4f31efc43fc8e30644814da
Dohun Kim <dohun8758@naver.com>
2026-01-14 22:26:07 +0900

    [Chore] #40 가독성을 위한 개행문자 추가

--- src/main/java/org/sopt/kareer/domain/member/dto/request/MemberOnboardRequest.java
+++ src/main/java/org/sopt/kareer/domain/member/dto/request/MemberOnboardRequest.java
@@ -14,30 +14,44 @@ import org.sopt.kareer.domain.member.entity.enums.VisaType;
 public record MemberOnboardRequest(
         @NotBlank(message = "이름은 필수 입력값입니다.")
         String name,
+
         @NotNull(message = "생년월일은 필수 입력값입니다.")
         LocalDate birthDate,
+
         @NotNull(message = "국가는 필수 입력값입니다.")
         Country country,
+
         @NotNull(message = "언어 능력은 필수 입력값입니다.")
         LanguageLevel languageLevel,
+
         @NotNull(message = "학위는 필수 입력값입니다.")
         Degree degree,
+
         @NotNull(message = "비자 유형은 필수 입력값입니다.")
         VisaType visaType,
+
         @Schema(description = "예상 졸업일, D2 비자인 경우만", type = "string", format = "date", example = "2025-08-31")
         LocalDate expectedGraduationDate,
+
         @NotNull(message = "비자 시작일은 필수 입력값입니다.")
         LocalDate visaStartDate,
+
         @NotNull(message = "비자 만료일은 필수 입력값입니다.")
         LocalDate visaExpiredAt,
+
         @Schema(description = "비자 점수, D10 비자인 경우만", example = "50")
         Integer visaPoint,
+
         @NotBlank(message = "제1전공은 필수 입력값입니다.")
         String primaryMajor,
+
         String secondaryMajor,
+
         @NotBlank(message = "희망 직무는 필수 입력값입니다.")
         String targetJob,
+
         String targetJobSkill,
+
         @NotBlank(message = "개인 배경은 필수 입력값입니다.")
         @Size(max = 1000, message = "개인 배경은 최대 1000자까지 입력할 수 있습니다.")
         String personalBackground
