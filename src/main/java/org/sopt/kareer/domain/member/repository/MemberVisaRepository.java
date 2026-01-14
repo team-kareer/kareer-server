d596645b42734b707b37a1ddee6baee903a2d160
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-15 03:04:13 +0900

    [Feat] #44 MemberVisa에 findAll 메서드 구현

--- src/main/java/org/sopt/kareer/domain/member/repository/MemberVisaRepository.java
+++ src/main/java/org/sopt/kareer/domain/member/repository/MemberVisaRepository.java
@@ -1,7 +1,11 @@
 package org.sopt.kareer.domain.member.repository;
 
 import org.sopt.kareer.domain.member.entity.MemberVisa;
+import org.sopt.kareer.domain.member.entity.enums.VisaStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberVisaRepository extends JpaRepository<MemberVisa, Long> {
+    List<MemberVisa> findAllByMemberIdAndVisaStatus(Long memberId, VisaStatus visaStatus);
 }
