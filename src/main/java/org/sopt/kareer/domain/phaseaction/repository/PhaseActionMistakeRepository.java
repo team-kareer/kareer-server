7e85ffe876b43a53fe8fbadf3868db0bb4c3bc3c
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-15 03:00:03 +0900

    [Feat] #44 PhaseAction에서 guideline, commonMistakes 분리

---
+++ src/main/java/org/sopt/kareer/domain/phaseaction/repository/PhaseActionMistakeRepository.java
@@ -0,0 +1,7 @@
+package org.sopt.kareer.domain.phaseaction.repository;
+
+import org.sopt.kareer.domain.phaseaction.entity.PhaseActionMistake;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhaseActionMistakeRepository extends JpaRepository<PhaseActionMistake, Long> {
+}
