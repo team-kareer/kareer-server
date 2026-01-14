616b44c93076a9b4dfdd65858d21b38823b2a38a
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-15 03:07:10 +0900

    [Feat] #44 LLM JSON 응답으로부터 엔티티 생성하는 로직 구현

---
+++ src/main/java/org/sopt/kareer/domain/member/service/RoadMapPersistService.java
@@ -0,0 +1,85 @@
+package org.sopt.kareer.domain.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.kareer.domain.actionitem.entity.ActionItem;
+import org.sopt.kareer.domain.actionitem.entity.enums.ActionItemType;
+import org.sopt.kareer.domain.actionitem.repository.ActionItemRepository;
+import org.sopt.kareer.domain.member.dto.response.RoadmapResponse;
+import org.sopt.kareer.domain.member.entity.Member;
+import org.sopt.kareer.domain.phase.entity.Phase;
+import org.sopt.kareer.domain.phase.entity.enums.PhaseStatus;
+import org.sopt.kareer.domain.phase.repository.PhaseRepository;
+import org.sopt.kareer.domain.phaseaction.entity.PhaseAction;
+import org.sopt.kareer.domain.phaseaction.entity.PhaseActionGuideline;
+import org.sopt.kareer.domain.phaseaction.entity.PhaseActionMistake;
+import org.sopt.kareer.domain.phaseaction.entity.enums.PhaseActionType;
+import org.sopt.kareer.domain.phaseaction.repository.PhaseActionGuidelineRepository;
+import org.sopt.kareer.domain.phaseaction.repository.PhaseActionMistakeRepository;
+import org.sopt.kareer.domain.phaseaction.repository.PhaseActionRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class RoadMapPersistService {
+
+    private final PhaseRepository phaseRepository;
+    private final PhaseActionRepository phaseActionRepository;
+    private final PhaseActionGuidelineRepository phaseActionGuidelineRepository;
+    private final PhaseActionMistakeRepository phaseActionMistakeRepository;
+    private final ActionItemRepository actionItemRepository;
+
+    @Transactional
+    public void saveRoadMap(Member member, RoadmapResponse response) {
+
+        for(RoadmapResponse.PhasePlan phasePlan : response.phases()){
+            Phase phase = Phase.create(
+                    member,
+                    phasePlan.sequence(),
+                    phasePlan.goal(),
+                    phasePlan.description(),
+                    PhaseStatus.valueOf(phasePlan.status()),
+                    LocalDate.parse(phasePlan.startDate()),
+                    LocalDate.parse(phasePlan.endDate())
+            );
+            Phase savedPhase = phaseRepository.save(phase);
+
+            for(RoadmapResponse.PhaseActionPlan phaseActionPlan : phasePlan.actions()){
+                PhaseAction phaseAction = PhaseAction.create(
+                        phaseActionPlan.title(),
+                        phaseActionPlan.description(),
+                        PhaseActionType.valueOf(phaseActionPlan.type()),
+                        LocalDate.parse(phaseActionPlan.deadline()),
+                        phaseActionPlan.importance(),
+                        savedPhase
+                );
+                PhaseAction savedPhaseAction = phaseActionRepository.save(phaseAction);
+
+                for(String g : phaseActionPlan.guideline()){
+                    PhaseActionGuideline phaseActionGuideline = PhaseActionGuideline.create(g, savedPhaseAction);
+                    phaseActionGuidelineRepository.save(phaseActionGuideline);
+                }
+
+                for(String c : phaseActionPlan.commonMistakes()){
+                    PhaseActionMistake phaseActionMistake = PhaseActionMistake.create(c, savedPhaseAction);
+                    phaseActionMistakeRepository.save(phaseActionMistake);
+                }
+
+                for(RoadmapResponse.ActionItemPlan actionItemPlan : phaseActionPlan.actionItems()){
+                    ActionItem actionItem = ActionItem.create(
+                            actionItemPlan.title(),
+                            ActionItemType.valueOf(actionItemPlan.actionsType()),
+                            LocalDate.parse(actionItemPlan.deadline()),
+                            member,
+                            savedPhaseAction
+                    );
+                    actionItemRepository.save(actionItem);
+                }
+
+            }
+
+        }
+    }
+}
