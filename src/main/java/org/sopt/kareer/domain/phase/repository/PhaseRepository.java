6c5d3bdcbadd36e1805ae98a95006c1f22aa0abc
hyomee2 <s5763305@naver.com>
2026-01-15 17:51:30 +0900

    [Feat] #41 Phase 리스트 조회 API 구현

--- src/main/java/org/sopt/kareer/domain/phase/repository/PhaseRepository.java
+++ src/main/java/org/sopt/kareer/domain/phase/repository/PhaseRepository.java
@@ -3,5 +3,6 @@ package org.sopt.kareer.domain.phase.repository;
 import org.sopt.kareer.domain.phase.entity.Phase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhaseRepository extends JpaRepository<Phase, Long> {
+public interface PhaseRepository extends JpaRepository<Phase, Long>, PhaseRepositoryCustom {
+
 }
