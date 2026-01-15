d233f9f08397a706da0df74fed560ec01a8f4065
hyomee2 <s5763305@naver.com>
2026-01-15 17:49:48 +0900

    [Refactor] #41 PhaseStatus에 workStatus 매핑하도록 리팩토링

--- src/main/java/org/sopt/kareer/domain/phase/entity/enums/PhaseStatus.java
+++ src/main/java/org/sopt/kareer/domain/phase/entity/enums/PhaseStatus.java
@@ -1,9 +1,31 @@
 package org.sopt.kareer.domain.phase.entity.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum PhaseStatus {
-    CURRENT,
-    NEXT,
-    FUTURE,
-    PREVIOUS,
-    PAST
+
+    PREVIOUS("Previous"),
+    PAST("Past"),
+    CURRENT("Current", "Remained works"),
+    NEXT("Next", "Scheduled works"),
+    FUTURE("Future", "Scheduled works");
+
+
+    private final String displayName;
+    private final String workStatus;
+
+    PhaseStatus(String displayName) {
+        this.displayName = displayName;
+        this.workStatus = null; // 남은 actions 수에 따라 동적으로 결정되므로 null
+    }
+
+    public static String determinieWorkStatus(PhaseStatus status, Long workCount) {
+        if (status == PhaseStatus.PREVIOUS || status == PhaseStatus.PAST) {
+            return workCount == 0 ? "All completed" : "Incomplete works";
+        }
+        return status.getWorkStatus();
+    }
 }
