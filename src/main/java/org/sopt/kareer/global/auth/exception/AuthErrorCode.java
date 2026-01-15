2643544a772bfb9587b1d76e67b9e44e108b5ab7
Dohun Kim <dohun8758@naver.com>
2026-01-16 01:14:43 +0900

    [Refactor] #52 예외, 에러코드 네이밍 변경

--- src/main/java/org/sopt/kareer/global/auth/exception/AuthErrorCode.java
+++ src/main/java/org/sopt/kareer/global/auth/exception/AuthErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum LoginErrorCode implements ErrorCode {
+public enum AuthErrorCode implements ErrorCode {
     LOGIN_CODE_NOT_FOUND(HttpStatus.UNAUTHORIZED.value(), "로그인 코드가 만료되었거나 존재하지 않습니다."),
     LOGIN_CODE_ALREADY_USED(HttpStatus.CONFLICT.value(), "이미 사용된 로그인 코드입니다.");
 
--- src/main/java/org/sopt/kareer/global/auth/exception/AuthException.java
+++ src/main/java/org/sopt/kareer/global/auth/exception/AuthException.java
@@ -2,13 +2,13 @@ package org.sopt.kareer.global.auth.exception;
 
 import org.sopt.kareer.global.exception.customexception.CustomException;
 
-public class LoginException extends CustomException {
+public class AuthException extends CustomException {
 
-    public LoginException(LoginErrorCode errorCode) {
+    public AuthException(AuthErrorCode errorCode) {
         super(errorCode);
     }
 
-    public LoginException(LoginErrorCode errorCode, String messageDetail) {
+    public AuthException(AuthErrorCode errorCode, String messageDetail) {
         super(errorCode, messageDetail);
     }
 }
