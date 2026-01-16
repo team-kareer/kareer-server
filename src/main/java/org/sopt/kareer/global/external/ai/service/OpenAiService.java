05668c5bbc3f66ee427bd8706d0c33c78b47549a
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-16 16:44:05 +0900

    [Feat] #56 채용 공고 추천 LLM 호출 로직 구현

--- src/main/java/org/sopt/kareer/global/external/ai/service/OpenAiService.java
+++ src/main/java/org/sopt/kareer/global/external/ai/service/OpenAiService.java
@@ -2,8 +2,12 @@ package org.sopt.kareer.global.external.ai.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.sopt.kareer.domain.roadmap.dto.response.RoadmapResponse;
-import org.sopt.kareer.global.external.ai.constant.OpenAiPrompt;
+import org.sopt.kareer.global.external.ai.constant.JobPostingRecommendPrompt;
+import org.sopt.kareer.global.external.ai.constant.RoadmapPrompt;
+import org.sopt.kareer.global.external.ai.dto.response.JobPostingRecommendResults;
+import org.sopt.kareer.global.external.ai.enums.RagType;
 import org.sopt.kareer.global.external.ai.exception.LlmErrorCode;
 import org.sopt.kareer.global.external.ai.exception.LlmException;
 import org.springframework.ai.chat.client.ChatClient;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OpenAiService {
@@ -22,35 +27,66 @@ public class OpenAiService {
     private final ObjectMapper objectMapper;
 
     public RoadmapResponse generateRoadmap(String memberContext, List<Document> retrievedDocument){
-        String context = buildRagContext(retrievedDocument);
+        String context = buildRagContext(retrievedDocument, RagType.DOCUMENT);
 
-        String systemPrompt = OpenAiPrompt.ROADMAP_SYSTEM_PROMPT;
+        String systemPrompt = RoadmapPrompt.ROADMAP_SYSTEM_PROMPT;
 
-        String userPrompt = OpenAiPrompt.ROADMAP_USER_PROMPT_FORMAT.formatted(LocalDate.now(), memberContext, context);
+        String userPrompt = RoadmapPrompt.ROADMAP_USER_PROMPT_FORMAT.formatted(LocalDate.now(), memberContext, context);
         
-        try {
+        return executeJson(systemPrompt, userPrompt, RoadmapResponse.class);
+    }
 
+    public List<Long> recommendJobPosting(String userContext, List<Document> retrievedDocument){
+        String ragContext = buildRagContext(retrievedDocument, RagType.JOBPOSTING);
+
+        String systemPrompt = JobPostingRecommendPrompt.JOB_POSTING_SYSTEM_PROMPT;
+        String userPrompt = JobPostingRecommendPrompt.JOB_POSTING_USER_PROMPT_FORMAT.formatted(userContext, ragContext);
+
+        JobPostingRecommendResults searched = executeJson(systemPrompt, userPrompt, JobPostingRecommendResults.class);
+
+        searched.results().forEach(i ->
+                log.info("[JOB_RECOMMEND] jobPostingId={}, reason={}", i.jobPostingId(), i.reason()));
+
+        return searched.results().stream()
+                .map(JobPostingRecommendResults.JobPostingRecommendResult::jobPostingId)
+                .toList();
+
+    }
+
+    private String buildRagContext(List<Document> docs, RagType ragType) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < docs.size(); i++) {
+            Document d = docs.get(i);
+            switch (ragType) {
+                case DOCUMENT:
+                    sb.append("### chunk ").append(i).append("\n");
+                    sb.append(d.getText()).append("\n\n");
+                    break;
+                case JOBPOSTING:
+                    sb.append("### candidate ").append(i).append("\n");
+                    Object jobPostingId = d.getMetadata().get("jobPostingId");
+                    sb.append("jobPostingId=").append(jobPostingId).append("\n");
+                    sb.append(d.getText()).append("\n\n");
+
+            }
+        }
+        return sb.toString();
+    }
+
+    private <T> T executeJson(String systemPrompt, String userPrompt, Class<T> clazz) {
+        try {
             ChatClient chatClient = chatClientBuilder.build();
+
             String responseJson = chatClient
                     .prompt()
                     .system(systemPrompt)
                     .user(userPrompt)
                     .call()
                     .content();
-            
-            return objectMapper.readValue(responseJson, RoadmapResponse.class);
+
+            return objectMapper.readValue(responseJson, clazz);
         } catch (Exception e) {
             throw new LlmException(LlmErrorCode.LLM_JSON_PARSING_FAILED, e.getMessage());
         }
-    }
-
-    private String buildRagContext(List<Document> docs) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < docs.size(); i++) {
-            Document d = docs.get(i);
-            sb.append("### chunk ").append(i).append("\n");
-            sb.append(d.getText()).append("\n\n");
-        }
-        return sb.toString();
     }
 }
