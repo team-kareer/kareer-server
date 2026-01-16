46859ae9bd20dba1f3b1dea8cbda050114cfe629
hyomee2 <s5763305@naver.com>
2026-01-16 18:40:42 +0900

    [Refactor] #64 PhaseActionType 포맷팅 추가(첫글자 대문자, 이외 소문자)

--- src/main/java/org/sopt/kareer/domain/roadmap/dto/response/HomePhaseActionResponse.java
+++ src/main/java/org/sopt/kareer/domain/roadmap/dto/response/HomePhaseActionResponse.java
@@ -1,6 +1,7 @@
 package org.sopt.kareer.domain.roadmap.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.sopt.kareer.domain.roadmap.entity.Phase;
 import org.sopt.kareer.domain.roadmap.entity.enums.PhaseActionType;
 
 import java.time.LocalDate;
@@ -8,7 +9,7 @@ import java.time.LocalDate;
 @Schema(description = "홈 Phase Action 조회 응답")
 public record HomePhaseActionResponse(
         @Schema(description = "Phase Action 타입", example="Visa")
-        PhaseActionType type,
+        String type,
 
         @Schema(description = "Phase Action 제목", example="Prepare OPT application documents")
         String title,
@@ -16,4 +17,12 @@ public record HomePhaseActionResponse(
         @Schema(description = "Phase Action 마감기한", example="2026-01-01")
         LocalDate deadline
 ) {
+        private static String formatType(PhaseActionType type) {
+                String lowerCase = type.name().toLowerCase();
+                return lowerCase.substring(0, 1).toUpperCase() + lowerCase.substring(1);
+        }
+
+        public HomePhaseActionResponse(PhaseActionType type, String title, LocalDate deadline) {
+                this(formatType(type), title, deadline);
+        }
 }
