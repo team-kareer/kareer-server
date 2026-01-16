0cab7a95b2d0a37d46a0343f90720ca0519a2399
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-16 15:40:21 +0900

    [Feat] #56 JobPosting 도메인 예외 코드, 예외 클래스 구현

--- src/main/java/org/sopt/kareer/domain/jobposting/exception/JobPostingErrorCode.java
+++ src/main/java/org/sopt/kareer/domain/jobposting/exception/JobPostingErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum JobPostingErrorCode implements ErrorCode {
-    JOB_POSTING_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 채용 공고입니다.")
+    JOB_POSTING_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 채용 공고입니다."),
+    RESUME_CONTEXT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR.value(), "이력서/자소서 기반 사용자 컨텍스트 생성에 실패했습니다."),
+    TOO_MANY_FILES(HttpStatus.BAD_REQUEST.value(), "이력서/자소서는 최대 2개 업로드 가능합니다.")
     ;
 
     private final int httpStatus;
--- src/main/java/org/sopt/kareer/domain/jobposting/exception/JobPostingException.java
+++ src/main/java/org/sopt/kareer/domain/jobposting/exception/JobPostingException.java
@@ -3,7 +3,11 @@ package org.sopt.kareer.domain.jobposting.exception;
 import org.sopt.kareer.global.exception.customexception.CustomException;
 
 public class JobPostingException extends CustomException {
+
     public JobPostingException(JobPostingErrorCode errorCode) {
         super(errorCode);
+    }
+    public JobPostingException(JobPostingErrorCode errorCode, String message) {
+        super(errorCode, message);
     }
 }
