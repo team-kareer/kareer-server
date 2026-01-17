b72076fb05ca5b9453dbf99af3fef49c5661488f
Dohun Kim <dohun8758@naver.com>
2026-01-17 15:42:31 +0900

    [Docs] #66 스웨거 명세 추가

--- src/main/java/org/sopt/kareer/domain/roadmap/dto/response/ActionItemResponse.java
+++ src/main/java/org/sopt/kareer/domain/roadmap/dto/response/ActionItemResponse.java
@@ -1,12 +1,18 @@
 package org.sopt.kareer.domain.roadmap.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import org.sopt.kareer.domain.roadmap.entity.ActionItem;
 
+@Schema(description = "Action Item 응답(Todo)")
 public record ActionItemResponse(
+        @Schema(description = "Action Item id", example = "1")
         Long actionItemId,
+        @Schema(description = "Action Item 제목", example = "Prepare Resume")
         String title,
+        @Schema(description = "Action Item 마감일", example = "2025-10-15")
         LocalDate deadline,
+        @Schema(description = "Action Item 완료 여부", example = "false")
         boolean completed
 ) {
 
