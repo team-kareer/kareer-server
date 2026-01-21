d817b1cc179f3ba3ab1fa74db9eb6e9ae83cd28b
dlwjddus1112 <dlwjddus1112@naver.com>
2026-01-21 20:40:53 +0900

    [Feat] #106 VisaType에 description 필드 추가

--- src/main/java/org/sopt/kareer/domain/member/entity/enums/VisaType.java
+++ src/main/java/org/sopt/kareer/domain/member/entity/enums/VisaType.java
@@ -1,6 +1,15 @@
 package org.sopt.kareer.domain.member.entity.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum VisaType {
-    D2,
-    D10
+    D2("D-2"),
+    D10("D-10"),
+    E7("E-7")
+    ;
+
+    private final String description;
 }
