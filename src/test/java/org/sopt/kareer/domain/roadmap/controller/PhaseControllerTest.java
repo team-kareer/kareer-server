2209ab70722f69073a204b35c6ab0674f293f43c
hyomee2 <s5763305@naver.com>
2026-03-03 03:45:23 +0900

    [Test] #171 Phase 리스트 조회 Controller 테스트 코드 작성

---
+++ src/test/java/org/sopt/kareer/domain/roadmap/controller/PhaseControllerTest.java
@@ -0,0 +1,48 @@
+package org.sopt.kareer.domain.roadmap.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sopt.kareer.domain.roadmap.dto.response.PhaseListResponse;
+import org.sopt.kareer.domain.roadmap.dto.response.PhaseResponse;
+import org.sopt.kareer.domain.roadmap.fixture.PhaseResponseFixture;
+import org.sopt.kareer.domain.roadmap.service.PhaseService;
+import org.sopt.kareer.support.ControllerTestSupport;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PhaseController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class PhaseControllerTest extends ControllerTestSupport {
+
+    @MockBean
+    protected PhaseService phaseService;
+
+    @DisplayName("Phase 리스트를 성공적으로 조회한다.")
+    @Test
+    void getPhaseList_success() throws Exception {
+        // given
+        PhaseResponse phase1 = PhaseResponseFixture.of();
+        PhaseResponse phase2 = PhaseResponseFixture.of();
+        PhaseResponse phase3 = PhaseResponseFixture.of();
+
+        given(phaseService.getPhases(any()))
+                .willReturn(new PhaseListResponse(List.of(phase1, phase2, phase3)));
+
+        // when & then
+        mockMvc.perform(get("/api/v1/phases"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Phase 리스트가 조회되었습니다."))
+                .andExpect(jsonPath("$.data.phases").isArray())
+                .andExpect(jsonPath("$.data.phases.length()").value(3));
+    }
+}
