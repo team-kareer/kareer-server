459b96defab09d7be594507216626b3bbab5976e
hyomee2 <s5763305@naver.com>
2026-03-05 05:22:08 +0900

    [Test] #171 중복 테스트 코드 삭제

--- src/test/java/org/sopt/kareer/domain/roadmap/repository/PhaseRepositoryTest.java
+++ src/test/java/org/sopt/kareer/domain/roadmap/repository/PhaseRepositoryTest.java
@@ -165,28 +165,4 @@ public class PhaseRepositoryTest {
             assertThat(result).isFalse();
         }
     }
-
-    @Test
-    @DisplayName("phaseId가 일치하는 phaseAction들 중 아직 완료되지 않은 phaseAction들을 반환한다. ")
-    void findByPhaseIdAndCompletedIsFalse() {
-        // given
-        Phase phase2 = phaseRepository.save(PhaseFixture.getPhase(member1, 2, PhaseStatus.NEXT));
-
-        PhaseAction action1 = PhaseActionFixture.getPhaseAction(phase1, PhaseActionType.VISA);
-        PhaseAction action2 = PhaseActionFixture.getPhaseAction(phase1, PhaseActionType.VISA);
-        action2.markCompleted();
-        PhaseAction action3 = PhaseActionFixture.getPhaseAction(phase2, PhaseActionType.VISA);
-        phaseActionRepository.saveAll(List.of(action1, action2, action3));
-
-        // when
-        List<PhaseAction> result = phaseActionRepository.findByPhaseIdAndCompletedIsFalse(phase1.getId());
-
-        // then
-        assertThat(result).hasSize(1);
-        assertThat(result)
-                .extracting(PhaseAction::getId)
-                .containsExactly(action1.getId());
-        assertThat(result.get(0).getCompleted()).isFalse();
-        assertThat(result.get(0).getPhase().getId()).isEqualTo(phase1.getId());
-    }
 }
