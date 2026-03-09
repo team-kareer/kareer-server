e52558de75469202df9da32a7bf96d943b806739
dlwjddus1112 <dlwjddus1112@naver.com>
2026-03-09 21:04:31 +0900

    [Test] #181 MemberFixture에 학위, 대학, 영어 수준 추가

--- src/test/java/org/sopt/kareer/domain/member/fixture/MemberFixture.java
+++ src/test/java/org/sopt/kareer/domain/member/fixture/MemberFixture.java
@@ -1,16 +1,17 @@
 package org.sopt.kareer.domain.member.fixture;
 
 import org.sopt.kareer.domain.member.entity.Member;
-import org.sopt.kareer.domain.member.entity.enums.Country;
-import org.sopt.kareer.domain.member.entity.enums.MemberStatus;
-import org.sopt.kareer.domain.member.entity.enums.OAuthProvider;
-import org.sopt.kareer.domain.member.entity.enums.RoadmapStatus;
+import org.sopt.kareer.domain.member.entity.enums.*;
 
 public class MemberFixture {
     public static final String MEMBER_NAME = "test-user-name";
     public static final OAuthProvider MEMBER_OAUTH_PROVIDER = OAuthProvider.GOOGLE;
     public static final String MEMBER_PROVIDER_ID = "test-provider-id";
     public static final Country MEMBER_COUNTRY = Country.AFGHANISTAN;
+    public static final Degree MEMBER_DEGREE = Degree.DOMESTIC_ASSOCIATE;
+    public static final String MEMBER_UNIVERSITY = "University";
+    public static final EnglishLevel MEMBER_ENGLISH_LEVEL = EnglishLevel.ADVANCED;
+
 
 
     public static Member getMember() {
@@ -25,6 +26,9 @@ public class MemberFixture {
                 .providerId(providerId)
                 .status(MemberStatus.ACTIVE)
                 .roadmapStatus(RoadmapStatus.NOT_STARTED)
+                .degree(MEMBER_DEGREE)
+                .englishLevel(MEMBER_ENGLISH_LEVEL)
+                .university(MEMBER_UNIVERSITY)
                 .build();
     }
 }
