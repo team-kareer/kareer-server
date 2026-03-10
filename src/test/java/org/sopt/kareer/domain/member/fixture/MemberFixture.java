ec67c57c3b6c3a7ef4c956bb6ff6366ad86fae59
dlwjddus1112 <dlwjddus1112@naver.com>
2026-03-10 15:16:20 +0900

    [Test] #183 MemberFixture에 생년월일을 받는 메서드 오버로딩

--- src/test/java/org/sopt/kareer/domain/member/fixture/MemberFixture.java
+++ src/test/java/org/sopt/kareer/domain/member/fixture/MemberFixture.java
@@ -3,6 +3,8 @@ package org.sopt.kareer.domain.member.fixture;
 import org.sopt.kareer.domain.member.entity.Member;
 import org.sopt.kareer.domain.member.entity.enums.*;
 
+import java.time.LocalDate;
+
 public class MemberFixture {
     public static final String MEMBER_NAME = "test-user-name";
     public static final OAuthProvider MEMBER_OAUTH_PROVIDER = OAuthProvider.GOOGLE;
@@ -29,6 +31,21 @@ public class MemberFixture {
                 .degree(MEMBER_DEGREE)
                 .englishLevel(MEMBER_ENGLISH_LEVEL)
                 .university(MEMBER_UNIVERSITY)
+                .build();
+    }
+
+    public static Member getMember(LocalDate birthDate){
+        return Member.builder()
+                .name(MEMBER_NAME)
+                .country(MEMBER_COUNTRY)
+                .provider(MEMBER_OAUTH_PROVIDER)
+                .providerId(MEMBER_PROVIDER_ID)
+                .status(MemberStatus.ACTIVE)
+                .roadmapStatus(RoadmapStatus.NOT_STARTED)
+                .degree(MEMBER_DEGREE)
+                .englishLevel(MEMBER_ENGLISH_LEVEL)
+                .university(MEMBER_UNIVERSITY)
+                .birthDate(birthDate)
                 .build();
     }
 }
