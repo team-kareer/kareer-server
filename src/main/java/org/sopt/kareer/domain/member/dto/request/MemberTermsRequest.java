a86a9684569329be141266ec06e7b236b4878bc1
hyomee2 <s5763305@naver.com>
2026-03-27 02:29:05 +0900

    [Chore] #202 MemberTermRequest에 @NotNull 제약조건 추가

--- src/main/java/org/sopt/kareer/domain/member/dto/request/MemberTermsRequest.java
+++ src/main/java/org/sopt/kareer/domain/member/dto/request/MemberTermsRequest.java
@@ -11,7 +11,7 @@ import java.util.List;
 public record MemberTermsRequest(
         @Schema(description = "약관 동의 리스트")
         @NotEmpty(message = "약관 동의 목록은 비어있을 수 없습니다.")
-        List<@Valid TermAgreement> agreements
+        List<@NotNull(message = "약관 동의 항목은 null일 수 없습니다.") @Valid TermAgreement> agreements
 ) {
     public record TermAgreement(
             @Schema(description = "약관 고유번호", example="1")
@@ -20,6 +20,6 @@ public record MemberTermsRequest(
 
             @Schema(description = "약관 동의여부", example="true")
             @NotNull(message = "동의 여부는 필수입니다.")
-            Boolean agreed
+            boolean agreed
     ) {}
 }
