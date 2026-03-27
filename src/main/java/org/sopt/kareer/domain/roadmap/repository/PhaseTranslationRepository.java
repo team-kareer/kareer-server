26c735e779960b80bc735f9a627888e45a6e99eb
dlwjddus1112 <dlwjddus1112@naver.com>
2026-03-27 16:52:33 +0900

    [Feat] #214 번역 DB로부터 언어별 데이터를 가져오는 로직 구현

---
+++ src/main/java/org/sopt/kareer/domain/roadmap/repository/PhaseTranslationRepository.java
@@ -0,0 +1,14 @@
+package org.sopt.kareer.domain.roadmap.repository;
+
+import org.sopt.kareer.domain.roadmap.entity.PhaseTranslation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhaseTranslationRepository extends JpaRepository<PhaseTranslation, Long> {
+
+    List<PhaseTranslation> findAllByPhase_IdInAndLanguage(List<Long> phaseIds, String language);
+
+    void deleteAllByPhase_IdInAndLanguage(List<Long> phaseIds, String language);
+
+}
