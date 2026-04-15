cf0793cea24537f964aef91f7f895702bc52dd92
dlwjddus1112 <dlwjddus1112@naver.com>
2026-04-15 21:02:06 +0900

    [Refactor] #238 Phase 조회 시 상태 기준으로 조회하도록 수정

--- src/main/java/org/sopt/kareer/domain/roadmap/repository/PhaseRepository.java
+++ src/main/java/org/sopt/kareer/domain/roadmap/repository/PhaseRepository.java
@@ -1,11 +1,14 @@
 package org.sopt.kareer.domain.roadmap.repository;
 
 import org.sopt.kareer.domain.roadmap.entity.Phase;
+import org.sopt.kareer.domain.roadmap.entity.enums.RoadmapActiveStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhaseRepository extends JpaRepository<Phase, Long>, PhaseRepositoryCustom {
 
     boolean existsByIdAndRoadmap_Member_Id(Long phaseId, Long memberId);
+
+    boolean existsByIdAndRoadmap_Member_IdAndRoadmap_Status(Long phaseId, Long memberId, RoadmapActiveStatus status);
 
     void deleteAllByRoadmap_Member_Id(Long memberId);
 }
