3460ce9969ae4d4332c020cda6595ec4c238dc3e
dlwjddus1112 <dlwjddus1112@naver.com>
2026-04-23 15:14:22 +0900

    [Refactor] #244 북마크 조회 시 fetch join 설정

--- src/main/java/org/sopt/kareer/domain/jobposting/service/JobPostingQueryService.java
+++ src/main/java/org/sopt/kareer/domain/jobposting/service/JobPostingQueryService.java
@@ -2,7 +2,6 @@ package org.sopt.kareer.domain.jobposting.service;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.kareer.domain.jobposting.entity.JobPosting;
-import org.sopt.kareer.domain.jobposting.entity.JobPostingBookmark;
 import org.sopt.kareer.domain.jobposting.repository.JobPostingBookmarkRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,10 +16,6 @@ public class JobPostingQueryService {
     private final JobPostingBookmarkRepository jobPostingBookmarkRepository;
 
     public List<JobPosting> getBookmarkedJobPostings(Long memberId) {
-        return jobPostingBookmarkRepository
-                .findAllByMemberId(memberId)
-                .stream()
-                .map(JobPostingBookmark::getJobPosting)
-                .toList();
+        return jobPostingBookmarkRepository.findAllByMemberId(memberId);
     }
 }
